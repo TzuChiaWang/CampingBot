04f5d1a142e03ad08153f6f179af1806df7ae436
TzuChiaWang <tommywang901@gmail.com>
2025-04-18 13:51:39 +0800

    fix guni.con

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -3,37 +3,43 @@ import multiprocessing
 # 綁定的 IP 和端口
 bind = "0.0.0.0:3000"
 
-# 工作進程數
-workers = multiprocessing.cpu_count() * 2 + 1
+# 減少工作進程數，使用固定數量而不是基於 CPU
+workers = 2
 
-# 工作模式
-worker_class = "sync"
+# 工作模式改為更輕量的 gthread
+worker_class = "gthread"
+threads = 4
 
-# 超時時間
-timeout = 120
+# 增加超時時間
+timeout = 300
 
-# 是否守護進程
+# 不使用守護進程
 daemon = False
 
 # 日誌級別
 loglevel = "info"
 
-# 訪問日誌格式
-accesslog = "-"  # "-" 表示標準輸出
-errorlog = "-"   # "-" 表示標準輸出
+# 日誌輸出到標準輸出
+accesslog = "-"
+errorlog = "-"
 
 # 進程名稱
-proc_name = "camping_bot"
+proc_name = "camping-bot"
 
-# 最大客戶端並發數量
-worker_connections = 1000
+# 降低並發連接數
+worker_connections = 500
 
-# 限制每個工作進程處理的請求數
-max_requests = 2000
-max_requests_jitter = 400
+# 降低每個工作進程的最大請求數
+max_requests = 1000
+max_requests_jitter = 200
 
-# 優雅的工作模式
-graceful_timeout = 30
+# 優雅關閉時間
+graceful_timeout = 60
 
-# 預加載應用
-preload_app = True
+# 不預加載應用以節省內存
+preload_app = False
+
+# 限制緩衝區大小
+limit_request_line = 4096
+limit_request_fields = 100
+limit_request_field_size = 8190
