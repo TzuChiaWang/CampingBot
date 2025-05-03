9c9f441b330496a60cbdb29ab48ccf90b62e6d90
TzuChiaWang <tommywang901@gmail.com>
2025-05-03 18:19:52 +0800

    fix 301 problem

--- sitemap.py
+++ sitemap.py
@@ -1,32 +1,13 @@
-from flask import Blueprint, Response, url_for
-from datetime import datetime
+from flask import Blueprint, Response
 
 sitemap_bp = Blueprint("sitemap", __name__)
 
 
-@sitemap_bp.route("/sitemap.xml", methods=["GET"])
+@sitemap_bp.route("/sitemap.xml")
 def sitemap():
-    pages = []
     base_url = "https://camping.ddnsking.com"
-
-    now = datetime.utcnow().date().isoformat()
-
-    # 靜態頁面
-    static_pages = ["index", "login", "about"]  # 如有更多可加
-    for page in static_pages:
-        pages.append({"loc": url_for(page, _external=True), "lastmod": now})
-
-    sitemap_xml = f"""<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-"""
-    for page in pages:
-        sitemap_xml += f"""  <url>
-    <loc>{page["loc"]}</loc>
-    <lastmod>{page["lastmod"]}</lastmod>
-    <priority>0.8</priority>
-  </url>
-"""
-
-    sitemap_xml += "</urlset>"
-
-    return Response(sitemap_xml, mimetype="application/xml")
+    xml = '<?xml version="1.0" encoding="UTF-8"?>\n'
+    xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
+    xml += f"  <url><loc>{base_url}/</loc></url>\n"
+    xml += "</urlset>"
+    return Response(xml, mimetype="application/xml")
